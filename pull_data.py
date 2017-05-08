36d5b033119529e479acd77369cf24d1da81a87e
Austin Chi <austinchi2@yahoo.com>
2017-05-07 22:18:05 -0700

    fixed pull_data.py, added arguments

--- pull_data.py
+++ pull_data.py
@@ -1,12 +1,15 @@
 from grab_data import *
 import pandas as pd
+import sys 
 
 
-csv = pd.read_csv('./SF_location.csv')
-geom = csv['Geom']
+def main():
+    start = int(sys.argv[1])
+    stop = int(sys.argv[2])
+    csv = pd.read_csv('./SF_location.csv')
+    geom = csv['Geom']
+    for g in geom.iloc[start:stop]:
+        GetStreet(g, 90, "test")
 
-
-for g in geom:
-    GetStreet(g, 90, "test")
-
-
+if __name__ == "__main__":
+    main()
