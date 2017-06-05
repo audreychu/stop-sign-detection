004f3beefd7fe2f8d3f02f1c91da8083a936a988
Jeremy <jeweidner@ucdavis.edu>
2017-06-05 02:33:21 -0700

    changed data format added model save/load

--- dataprocess.py
+++ dataprocess.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import numpy as np
-#import tflearn as tfl
 import random
 import sklearn
 from sklearn.cross_validation import train_test_split
-#apparently it's called something else in my version of sklearn
+#apparently it's called something else in my version of sklearn idk
 #from sklearn.model_selection import train_test_split
 import os
 from PIL import Image
@@ -18,31 +17,50 @@ from tflearn.layers.estimator import regression
 
 os.chdir("C://Users/Jeremy/Desktop/School/160")
 
+testimage = Image.open('resizedyes/(37.71791711, -122.466248488)180.jpg')
+testimagec = testimage.copy()
+testimagec.show()
+t = np.array(testimagec)
 
+yespathlist = []
+for p in glob.glob('resizedyes/*.jpg'):
+    yespathlist.append(p)
+    
+nopathlist = []
+for p in glob.glob('resizedno/*.jpg'):
+    nopathlist.append(p)
+#nopathlist=random.shuffle(nopathlist)
+nopathlist = nopathlist[0:len(yespathlist)]
+
+allpath = yespathlist + nopathlist
+
+labellist = []
 image_list = []
-for path in ['resizedyes/*.jpg', 'resizedno/*.jpg']:
+for path in allpath:
     for filename in glob.glob(path):
         if len(image_list) < 1600:
-            im=Image.open(filename)
-            c = im.copy()
-            image_list.append(c)
-            im.close()
+                if len(image_list) < 789:
+                    label = [1, 0]
+                else:
+                    label = [0, 1]
+                im=Image.open(filename)
+                c = im.copy()
+                image_list.append(np.array(c))
+                labellist.append(np.array(label))
+                im.close()
+
+
 
 
 #789 are yes
-a = np.zeros([1,len(image_list)])
-for i in range(789):
-    a[0][i] = 1
 
-    
-b = a.tolist()[0]
-df = pd.DataFrame(image_list, columns=['image'])
-df['label'] = b
 ###END DATA PROCESSING###
-#Split train test 
-xtrain, xtest, ytrain, ytest = train_test_split(df['image'], df['label'], test_size = 0.2)
+#Split train test validate
+xtrain, x2, ytrain, y2 = train_test_split(image_list, labellist, test_size = 0.2)
+xvalid, xtest, yvalid, ytest = train_test_split(x2, y2, test_size = 0.5)
 
 
+#start of building net
 convnet = input_data(shape = [None, 640, 436, 3], name = 'input')
 
 convnet = conv_2D(incoming = convnet, nb_filter = 32, filter_size =  2, activation = 'relu', name = 'layer1')
@@ -66,10 +84,16 @@ convnet = dropout(convnet, keep_prob = 0.8, name = 'dropout')
 convnet = fully_connected(convnet, n_units = 2,activation = 'softmax', name = 'fullyconnected2')
 convnet = regression(convnet, optimizer = 'adam', loss='categorical_crossentropy', learning_rate=0.001, name = 'regression')
 
+model = tflearn.DNN(convnet, tensorboard_dir = '/tmp/tflearn_logs/')
 
 #fits model
-model = tflearn.DNN(convnet, tensorboard_dir = '/tmp/tflearn_logs/')
-model.fit({'input': X}, {'targets': Y}, n_epoch = 10, snapshot_epoch = True, run_id = 'stopsign', 
-          snapshot_steps = '200', validation_set=({'input': test_x}, {'targets': test_y}))
+if os.path.exists('stopsign.meta'):
+    model.load('stopsign')
+    print 'model loaded'
 
-model.save('tflearnsimpleCNN.model')
+else:
+    model.fit({'input': xtrain}, {'targets': ytrain}, n_epoch = 10, snapshot_epoch = True, run_id = 'stopsign', 
+              snapshot_steps = '200', validation_set=({'input': xvalid}, {'targets': yvalid}))
+    
+    
+    model.save('C://Users/Jeremy/Desktop/School/160/tflearnsimpleCNN.model')
