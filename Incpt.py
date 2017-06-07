e1440480702ee2b6038c1f246b24775f42b49fe0
Austin Chi <austinchi2@yahoo.com>
2017-06-06 23:30:53 -0700

    Added mini inception model called Incpt.py

---
+++ Incpt.py
@@ -0,0 +1,175 @@
+import pandas as pd
+import numpy as np
+import random
+import sklearn
+from sklearn.cross_validation import train_test_split
+#apparently it's called something else in my version of sklearn idk
+#from sklearn.model_selection import train_test_split
+import os
+from PIL import Image
+import glob
+
+import tflearn
+from tflearn.layers.normalization import local_response_normalization
+from tflearn.layers.conv import conv_2d, conv_2d_transpose, max_pool_2d, avg_pool_2d, upsample_2d, conv_1d, max_pool_1d, avg_pool_1d, residual_block, residual_bottleneck, conv_3d, max_pool_3d, avg_pool_3d, highway_conv_1d, highway_conv_2d, global_avg_pool, global_max_pool
+from tflearn.layers.core import input_data, dropout, fully_connected
+from tflearn.layers.estimator import regression
+from tflearn.layers.merge_ops import merge
+
+
+#os.chdir("C://Users/Jeremy/Desktop/School/160")
+#os.chdir("C:\\Users\\Austin Chi\\Google Drive\\Other\\misc")
+'''
+testimage = Image.open('resizedyes/(37.71791711, -122.466248488)180.jpg')
+testimagec = testimage.copy()
+testimagec.show()
+t = np.array(testimagec)'''
+
+yespathlist = []
+for p in glob.glob('tinyresizedyes/*.jpg'):
+    yespathlist.append(p)
+    
+nopathlist = []
+for p in glob.glob('tinyresizedno/*.jpg'):
+    nopathlist.append(p)
+#nopathlist=random.shuffle(nopathlist)
+nopathlist = nopathlist[0:len(yespathlist)]
+
+allpath = yespathlist + nopathlist
+
+labellist = []
+image_list = []
+for path in allpath:
+    for filename in glob.glob(path):
+            if len(image_list) < len(yespathlist):
+                label = 1
+            else:
+                label = 0
+            im=Image.open(filename)
+            c = im.copy()
+            image_list.append(np.array(c))
+            labellist.append(label)
+            im.close()
+labellist = np.array(labellist)
+labellist = labellist.reshape(-1,1)
+enc = sklearn.preprocessing.OneHotEncoder(sparse = False)
+enc.fit(labellist)
+labellist = enc.transform(labellist)
+
+#789 are yes
+
+###END DATA PROCESSING###
+#Split train test validate
+xtrain, x2, ytrain, y2 = train_test_split(image_list, labellist, test_size = 0.2)
+xvalid, xtest, yvalid, ytest = train_test_split(x2, y2, test_size = 0.5)
+
+
+#start of building net
+network = input_data(shape=[None, 109, 160, 3])
+conv1_7_7 = conv_2d(network, 64, 7, strides=2, activation='relu', name = 'conv1_7_7_s2')
+pool1_3_3 = max_pool_2d(conv1_7_7, 3,strides=2)
+pool1_3_3 = local_response_normalization(pool1_3_3)
+conv2_3_3_reduce = conv_2d(pool1_3_3, 64,1, activation='relu',name = 'conv2_3_3_reduce')
+conv2_3_3 = conv_2d(conv2_3_3_reduce, 192,3, activation='relu', name='conv2_3_3')
+conv2_3_3 = local_response_normalization(conv2_3_3)
+pool2_3_3 = max_pool_2d(conv2_3_3, kernel_size=3, strides=2, name='pool2_3_3_s2')
+inception_3a_1_1 = conv_2d(pool2_3_3, 64, 1, activation='relu', name='inception_3a_1_1')
+inception_3a_3_3_reduce = conv_2d(pool2_3_3, 96,1, activation='relu', name='inception_3a_3_3_reduce')
+inception_3a_3_3 = conv_2d(inception_3a_3_3_reduce, 128,filter_size=3,  activation='relu', name = 'inception_3a_3_3')
+inception_3a_5_5_reduce = conv_2d(pool2_3_3,16, filter_size=1,activation='relu', name ='inception_3a_5_5_reduce' )
+inception_3a_5_5 = conv_2d(inception_3a_5_5_reduce, 32, filter_size=5, activation='relu', name= 'inception_3a_5_5')
+inception_3a_pool = max_pool_2d(pool2_3_3, kernel_size=3, strides=1, )
+inception_3a_pool_1_1 = conv_2d(inception_3a_pool, 32, filter_size=1, activation='relu', name='inception_3a_pool_1_1')
+
+# merge the inception_3a__
+inception_3a_output = merge([inception_3a_1_1, inception_3a_3_3, inception_3a_5_5, inception_3a_pool_1_1], mode='concat', axis=3)
+
+
+
+
+inception_3b_1_1 = conv_2d(inception_3a_output, 128,filter_size=1,activation='relu', name= 'inception_3b_1_1' )
+inception_3b_3_3_reduce = conv_2d(inception_3a_output, 128, filter_size=1, activation='relu', name='inception_3b_3_3_reduce')
+inception_3b_3_3 = conv_2d(inception_3b_3_3_reduce, 192, filter_size=3,  activation='relu',name='inception_3b_3_3')
+inception_3b_5_5_reduce = conv_2d(inception_3a_output, 32, filter_size=1, activation='relu', name = 'inception_3b_5_5_reduce')
+inception_3b_5_5 = conv_2d(inception_3b_5_5_reduce, 96, filter_size=5,  name = 'inception_3b_5_5')
+inception_3b_pool = max_pool_2d(inception_3a_output, kernel_size=3, strides=1,  name='inception_3b_pool')
+inception_3b_pool_1_1 = conv_2d(inception_3b_pool, 64, filter_size=1,activation='relu', name='inception_3b_pool_1_1')
+
+#merge the inception_3b_*
+inception_3b_output = merge([inception_3b_1_1, inception_3b_3_3, inception_3b_5_5, inception_3b_pool_1_1], mode='concat',axis=3,name='inception_3b_output')
+
+
+# In[ ]:
+
+pool3_3_3 = max_pool_2d(inception_3b_output, kernel_size=3, strides=2, name='pool3_3_3')
+inception_4a_1_1 = conv_2d(pool3_3_3, 192, filter_size=1, activation='relu', name='inception_4a_1_1')
+inception_4a_3_3_reduce = conv_2d(pool3_3_3, 96, filter_size=1, activation='relu', name='inception_4a_3_3_reduce')
+inception_4a_3_3 = conv_2d(inception_4a_3_3_reduce, 208, filter_size=3,  activation='relu', name='inception_4a_3_3')
+inception_4a_5_5_reduce = conv_2d(pool3_3_3, 16, filter_size=1, activation='relu', name='inception_4a_5_5_reduce')
+inception_4a_5_5 = conv_2d(inception_4a_5_5_reduce, 48, filter_size=5,  activation='relu', name='inception_4a_5_5')
+inception_4a_pool = max_pool_2d(pool3_3_3, kernel_size=3, strides=1,  name='inception_4a_pool')
+inception_4a_pool_1_1 = conv_2d(inception_4a_pool, 64, filter_size=1, activation='relu', name='inception_4a_pool_1_1')
+
+inception_4a_output = merge([inception_4a_1_1, inception_4a_3_3, inception_4a_5_5, inception_4a_pool_1_1], mode='concat', axis=3, name='inception_4a_output')
+
+
+
+
+
+inception_4b_1_1 = conv_2d(inception_4a_output, 160, filter_size=1, activation='relu', name='inception_4a_1_1')
+inception_4b_3_3_reduce = conv_2d(inception_4a_output, 112, filter_size=1, activation='relu', name='inception_4b_3_3_reduce')
+inception_4b_3_3 = conv_2d(inception_4b_3_3_reduce, 224, filter_size=3, activation='relu', name='inception_4b_3_3')
+inception_4b_5_5_reduce = conv_2d(inception_4a_output, 24, filter_size=1, activation='relu', name='inception_4b_5_5_reduce')
+inception_4b_5_5 = conv_2d(inception_4b_5_5_reduce, 64, filter_size=5,  activation='relu', name='inception_4b_5_5')
+
+inception_4b_pool = max_pool_2d(inception_4a_output, kernel_size=3, strides=1,  name='inception_4b_pool')
+inception_4b_pool_1_1 = conv_2d(inception_4b_pool, 64, filter_size=1, activation='relu', name='inception_4b_pool_1_1')
+
+inception_4b_output = merge([inception_4b_1_1, inception_4b_3_3, inception_4b_5_5, inception_4b_pool_1_1], mode='concat', axis=3, name='inception_4b_output')
+
+
+
+
+inception_4c_1_1 = conv_2d(inception_4b_output, 128, filter_size=1, activation='relu',name='inception_4c_1_1')
+inception_4c_3_3_reduce = conv_2d(inception_4b_output, 128, filter_size=1, activation='relu', name='inception_4c_3_3_reduce')
+inception_4c_3_3 = conv_2d(inception_4c_3_3_reduce, 256,  filter_size=3, activation='relu', name='inception_4c_3_3')
+inception_4c_5_5_reduce = conv_2d(inception_4b_output, 24, filter_size=1, activation='relu', name='inception_4c_5_5_reduce')
+inception_4c_5_5 = conv_2d(inception_4c_5_5_reduce, 64,  filter_size=5, activation='relu', name='inception_4c_5_5')
+
+inception_4c_pool = max_pool_2d(inception_4b_output, kernel_size=3, strides=1)
+inception_4c_pool_1_1 = conv_2d(inception_4c_pool, 64, filter_size=1, activation='relu', name='inception_4c_pool_1_1')
+
+inception_4c_output = merge([inception_4c_1_1, inception_4c_3_3, inception_4c_5_5, inception_4c_pool_1_1], mode='concat', axis=3,name='inception_4c_output')
+
+
+
+
+inception_4d_1_1 = conv_2d(inception_4c_output, 112, filter_size=1, activation='relu', name='inception_4d_1_1')
+inception_4d_3_3_reduce = conv_2d(inception_4c_output, 144, filter_size=1, activation='relu', name='inception_4d_3_3_reduce')
+inception_4d_3_3 = conv_2d(inception_4d_3_3_reduce, 288, filter_size=3, activation='relu', name='inception_4d_3_3')
+inception_4d_5_5_reduce = conv_2d(inception_4c_output, 32, filter_size=1, activation='relu', name='inception_4d_5_5_reduce')
+inception_4d_5_5 = conv_2d(inception_4d_5_5_reduce, 64, filter_size=5,  activation='relu', name='inception_4d_5_5')
+inception_4d_pool = max_pool_2d(inception_4c_output, kernel_size=3, strides=1,  name='inception_4d_pool')
+inception_4d_pool_1_1 = conv_2d(inception_4d_pool, 64, filter_size=1, activation='relu', name='inception_4d_pool_1_1')
+
+inception_4d_output = merge([inception_4d_1_1, inception_4d_3_3, inception_4d_5_5, inception_4d_pool_1_1], mode='concat', axis=3, name='inception_4d_output')
+
+
+
+
+loss = fully_connected(inception_4d_output, 2,activation='softmax')
+network = regression(loss, optimizer='momentum',
+                     loss='categorical_crossentropy',
+                     learning_rate=0.001)
+
+model = tflearn.DNN(network, tensorboard_dir = '/tmp/tflearn_logs/')
+
+#fits model
+'''if os.path.exists('stopsign.meta'):
+    model.load('stopsign')
+    print('model loaded')
+'''
+model.fit(xtrain,ytrain, n_epoch = 100, snapshot_epoch = True, run_id = 'stopsign', validation_set=(xvalid,yvalid),snapshot_step = 50, show_metric = True)
+
+    
+model.save('C://Users/Jeremy/Desktop/School/160/stopsign.model')
