b5451d1f174be2f78753caefe4a613cd372ca8c2
audreychu <audreychu27@gmail.com>
2017-06-06 22:56:06 -0700

    Code for HOG feature extraction

---
+++ Logis_Reg/HOG_Feature.py
@@ -0,0 +1,35 @@
+import cv2
+import glob as glob
+import matplotlib.pyplot as plt
+from skimage.feature import hog
+from skimage import data, color, exposure
+
+
+# Compute gradient for Stop Present (Logical = 1)
+df = pd.DataFrame()
+for name in glob.glob('/Users/audreychu/Documents/4th Year/STA160/StopYes/*jpg'):
+    jpg = cv2.imread(name)
+    image = color.rgb2gray(jpg)
+    fd, hog_image = hog(image, orientations=8, pixels_per_cell=(16, 16),
+                    cells_per_block=(1, 1), visualise=True)
+    fd = pd.DataFrame(fd).T
+    df = df.append(fd)
+
+df['Stop'] = [1] * len(df)
+
+# Compute gradient for Stop Absent (Logical = 0)
+df2 = pd.DataFrame()
+for name in glob.glob('/Users/audreychu/Documents/4th Year/STA160/StopNo/*jpg'):
+    jpg = cv2.imread(name)
+    image = color.rgb2gray(jpg)
+    fd, hog_image = hog(image, orientations=8, pixels_per_cell=(16, 16),
+                    cells_per_block=(1, 1), visualise=True)
+    fd = pd.DataFrame(fd).T
+    df2 = df2.append(fd)
+
+df2['Stop'] = [0] * len(df2)
+
+# Combine dataframes
+df_ = pd.concat([df,df2])
+df_ = df_.reset_index()
+df_.to_csv('hog_data.csv', index=False, encoding='utf-8')
